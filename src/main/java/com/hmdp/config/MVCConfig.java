29fe94505ebd4cb828771604554d30994a2afc96
zhuyuanzhuo <2728659553@qq.com>
2025-06-02 11:55:27 +0800

    利用session实现短信登录

---
+++ src/main/java/com/hmdp/config/MVCConfig.java
@@ -0,0 +1,28 @@
+package com.hmdp.config;
+
+import com.hmdp.Interceptor.LoginInterceptor;
+import com.hmdp.Interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+                        "/blog/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher/**"
+                );
+    }
+}
